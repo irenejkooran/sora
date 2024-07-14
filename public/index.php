81705958c64d4e701ab391dfd411c21e5e4ef613
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-07-14 17:22:39 +0530

    Add basic files

--- public/index.php
+++ public/index.php
@@ -1,3 +1,33 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>SORA</title>
+</head>
+<body>
+    <nav>SORA</nav>
+    <section>
+        <div>
+        <form action="" class="login">
+            <input type="text">
+            <input type="text">
+            <button>Log in</button>
+        </form>
+       </div>
+       <div>
+        <form action="" class="signup">
+            <input type="text">
+            <input type="text">
+            <input type="email">
+            <input type="passemail">
+            <input type="password">
+            <button>Sign up</button>
+        </form>
+       </div>
+
+    </section>
+    
+</body>
+</html>

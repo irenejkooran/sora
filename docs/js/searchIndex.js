b458079df532d2286418f980a8014e92b7e381e3
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-07-15 14:08:04 +0530

    fix: add documentation
    add documentation to user model file
    
    fix: change gitignore
    
    fix: user model file error
    
    add function keyword to the constructor
    
    add: docs for user model
    
    change gitignore
    
    fix: implement register function
    
    user registration code implemented
    todo: implement validateRegistration
    
    fix: change gitignore
    
    regenerate docs
    
    regenerate new documentation
    add generate_code.sh script for ai prompts
    
    add: database schema
    
    fix: add documentation
    
    add documentation to user model file
    
    fix: change gitignore
    
    fix: user model file error
    
    add function keyword to the constructor
    
    add: docs for user model
    
    change gitignore
    
    fix: implement register function
    
    user registration code implemented
    todo: implement validateRegistration
    
    add: database schema
    
    fix: add documentation
    
    add documentation to user model file
    
    fix: change gitignore
    
    fix: user model file error
    
    add function keyword to the constructor
    
    add: docs for user model
    
    change gitignore
    
    fix: implement register function
    
    user registration code implemented
    todo: implement validateRegistration
    
    fix: change gitignore
    
    regenerate docs
    
    regenerate new documentation
    add generate_code.sh script for ai prompts
    
    fix: add documentation
    
    add documentation to user model file
    
    fix: change gitignore
    
    fix: user model file error

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\User",
+            "name": "User",
+            "summary": "User\u0020class\u0020for\u0020handling\u0020user\u002Drelated\u0020operations",
+            "url": "classes/User.html"
+        },                {
+            "fqsen": "\\User\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructor\u0020for\u0020User\u0020Class",
+            "url": "classes/User.html#method___construct"
+        },                {
+            "fqsen": "\\User\u003A\u003Aregister\u0028\u0029",
+            "name": "register",
+            "summary": "Register\u0020a\u0020new\u0020user",
+            "url": "classes/User.html#method_register"
+        },                {
+            "fqsen": "\\User\u003A\u003Alogin\u0028\u0029",
+            "name": "login",
+            "summary": "Authenticate\u0020a\u0020user",
+            "url": "classes/User.html#method_login"
+        },                {
+            "fqsen": "\\User\u003A\u003AfindUserByEmail\u0028\u0029",
+            "name": "findUserByEmail",
+            "summary": "Find\u0020a\u0020user\u0020by\u0020email\u0020address",
+            "url": "classes/User.html#method_findUserByEmail"
+        },                {
+            "fqsen": "\\User\u003A\u003AvalidateUserRegistration\u0028\u0029",
+            "name": "validateUserRegistration",
+            "summary": "validate\u0020user\u0020registration\u0020data.",
+            "url": "classes/User.html#method_validateUserRegistration"
+        },                {
+            "fqsen": "\\User\u003A\u003A\u0024db",
+            "name": "db",
+            "summary": "",
+            "url": "classes/User.html#property_db"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
--- src/models/user.php
+++ src/models/user.php
@@ -1,25 +1,103 @@
-<?php
+<?php                                                                                     
+                                                                                      
+/** User class for handling user-related operations                                       
+*/                                                                                       
+class User {  
 
+		/** @var mysqli $db Database connection object */                                                  
+	  private $db;    
 
-class User {
-	private $db;
+		/**                                                                                     
+		* Constructor for User Class                                                                                                                                                 
+		* @param mysqli $db The database connection object 
+		* @return void                                    
+		*/                                                                                     
+		public function __construct(mysqli $db ): void {                                                        
+		$this->db = $db;                                                                        
+		} 
 
-	public __construct(Database $db) {
-	$this->db = $db;
-	}
+		/**                                                                                     
+		* Register a new user                                                                                                                                                     
+		* @param string[] $data An associative array containing user registration data.           
+		*                     Expected keys: 'firstName', 'LastName',        
+		*                                     'username', 'email',           
+		*                                     'password', 'confirmPassword'.                                                                                                                   
+		* @return (bool|string[]) An associative array with keys:                                        
+		*             'success' (bool) - Whether the registration was successful.              
+		*             'error' (string[])  - Any error messages if registration failed.            
+		*/                                                                                     
+		public function register(array $data): array {              
+      $validatedResult = validateUserRegistration($data);
+      if (!$validatedResult['isValid']) {
+				return [
+					'success' => 'false',
+					'error'   => $validatedResult['error'],
+				];
+			}
+			$username  = $data['username'];
+			$email = $data['email']
+			$firstName = $data['firstName'];
+			$lastName = $data['lastName'];
+			$password = $data['password'];
+			$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-	public function register($data) {
-		//unimplemented
-		//
-		//
-	}
+			$stmt = $this->db->prepare("insert into user('firstname', 'lastname', 'user', 'email', 'password') 
+				                          values(?,?,?,?,?)");
+			$stmt->bind_param("sssss", $firstName, $lastName, $username, $email, $hashed_password);
+			if($stmt->execute()){
+				return [
+					'success' => true,
+					'error'   => null,
+				];
+			}
+			else {
+				return [
+					'success' => false,
+					'error'   => ["cannot register user try again later."],
+			}
 
-	public function login($username, $password) {
-		//unimplemented
-	}
+		}                                                                                       
 
-	public function findUserByEmail($email) {
-		//unimplemented
-	}
-}
+		/**                                                                                     
+		* Authenticate a user                                                                  
+		* @param string $username The username of the user                                     
+		* @param string $password The 	password of the user                                    
+		* @return string[]|null An array of user data if login is successful or null if it fails. 
+		*/                                                                                     
+		public function login(string $username, string $password): ?array {                                   
+		//unimplemented                                                                       
+		}
 
+		/**                                                                                     
+		* Find a user by email address                                                         
+		* @param string $email email address to search for                                     
+		* return string[]|null An array of user data if found, or null if not found.              
+		*/                                                                                     
+		public function findUserByEmail(string $email): array|null {                                               
+		//unimplemented                                                                       
+		}                                                                                       
+	
+		/**                                                                                     
+		* validate user registration data.                                                     
+		* @param string[] $data An associative array containing user registration data.           
+		*                    Expected keys: 'firstName', 'LastName',         
+		*                                    'username', 'email' ,            
+		*                                     'password', 'confirmPassword'.                             
+		* @return (bool|string[])[] An associative array with keys:                                        
+		*               'isValid' (bool) - Whether the data is valid.                          
+		*                'error' (array) - Any validation error messages.                      
+		*/                                                                                     
+		private function validateUserRegistration(array $data): array {                                
+			$username = $data['username'];
+			$firstName = $data['firstName'];
+			$lastName = $data['lastName'];
+			$password = $data['password'];
+			$confirmPassword = $data['confirmPassword'];
+
+		} 
+
+}                                                                                         
+                                                                                      
+}                                                                                         
+                                                                                      
+?>

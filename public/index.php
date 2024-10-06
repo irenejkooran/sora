70b1555b0e1b3a214fe419b12b08e6da0c6ae01b
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-10-06 17:20:49 +0530

    fix: app routes

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ $app = new Application($router);
 $app->router->get('/', [HomeController::class, 'home']);
 $app->router->get('/login', [HomeController::class, 'login']);
 $app->router->post('/login', [UserController::class, 'login']);
-$app->router->get('/register', [UserController::class, 'register']);
+$app->router->get('/register', [HomeController::class, 'register']);
 $app->router->post('/register', [UserController::class, 'register']);
 $app->router->get('/logout', [UserController::class, 'logout']);
 

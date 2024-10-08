a54e47722739577369cb75a521fb5f7520817585
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-10-09 00:09:28 +0530

    feat: add theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ['./src/**/*.{html,php,js}',
     './**/*.{html,php,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          'sora-primary': '#4F46E5',
+          'sora-secondary': '#818CF8',
+          'sora-bg': '#F3F4F6',
+          'sora-text': '#1F2937',
+      }
+  },
   },
   plugins: [],
 }

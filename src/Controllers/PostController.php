a1abd7073f7bd12b3b4a0b6ab604088572a50cf0
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-10-12 19:13:20 +0530

    add: function to render tweets
    render tweets with the data passed

--- src/Controllers/PostController.php
+++ src/Controllers/PostController.php
@@ -41,6 +41,37 @@ class PostController {
             $errors[] = "invalid request method";
         }
     }
+
+    public static function render_tweet($username, $content, $created_at, $upvotes=0, $comments=0, $dp=NULL){
+
+        $html = <<<HTML
+    <div class="bg-white p-4 rounded-lg shadow hover:shadow-md transition duration-300">
+        <div class="flex items-center mb-2">
+            <img src="{$dp}" alt="User 1" class="w-10 h-10 rounded-full mr-3">
+            <div>
+                <a href="/" class="font-bold text-sora-secondary block">@{$username}</a>
+                <div class="flex items-center text-sm text-gray-500">
+                    <i class="fas fa-clock mr-1"></i>
+                    <span>{$created_at}</span>
+                </div>
+            </div>
+        </div>
+        <p class="mb-3">{$content}</p>
+        <div class="flex items-center space-x-4 text-gray-500">
+            <button class="flex items-center space-x-1 hover:text-blue-500 transition duration-300">
+                <i class="fas fa-arrow-up"></i>
+                <span>{$upvotes}</span>
+            </button>
+            <button class="flex items-center space-x-1 hover:text-green-500 transition duration-300">
+                <i class="fas fa-comment"></i>
+                <span>{$comments} comments</span>
+            </button>
+        </div>
+    </div>
+    HTML;
+    return $html;
+
+    }
 }
 
 ?>
